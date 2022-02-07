1b2c4efb1d50c169990d27d019cbbe99b98bc9fe
Piscode31 <66965290+Piscode31@users.noreply.github.com>
2022-02-07 15:56:16 +0100

    [FEAT] Add Attractable Script
    Created a component used for the G.O that is going to be attracted.

--- Assets/Scripts/Atractor/Atractor.cs
+++ Assets/Scripts/Atractor/Atractor.cs
@@ -1,10 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
 public class Attractor : MonoBehaviour
 {
